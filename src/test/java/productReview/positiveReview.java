18f86a5c32d56024204c3bbbf7a319ecc13fdf8a
Ayinla12 <ayinlaaibrahim@gmail.com>
2024-11-26 09:53:22 +0000

    Socond commit

--- src/test/java/productReview/positiveReview.java
+++ src/test/java/productReview/positiveReview.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -15,6 +17,7 @@ import java.time.Duration;
 
 public class positiveReview {
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeClass
     public void setup() {
@@ -23,14 +26,19 @@ public class positiveReview {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @Test(priority = 1)
     public void launchHomepage() {
         //Section: opens homepage & verify it loaded correctly
         driver.get("https://automationexercise.com/");
-        driver.findElement(By.xpath("//button[@aria-label='Consent']")).click();
+        try {
+            WebElement consentButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Consent']")));
+            consentButton.click();
+        }catch (Exception e){
+            System.out.println("Consent Button not displayed");
+        }
         String pageTitle = driver.getTitle();
         Assert.assertEquals(pageTitle, "Automation Exercise");
     }
